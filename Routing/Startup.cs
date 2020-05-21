fd20c107a2aeac80ebc448b975ee043b106ba253
Sandra Jacheva <sandra@transformify.digital>
2020-05-21 22:06:07 +0200

    removed unused namespaces

--- Routing/Startup.cs
+++ Routing/Startup.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+
 using System.Net.Http;
-using System.Threading.Tasks;
 using Blazor.FileReader;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,7 +16,7 @@ namespace Routing
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration) 
         {
             Configuration = configuration;
         }
